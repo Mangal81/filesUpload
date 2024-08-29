d75e78f062213cad51dadf56cc9956fe2e8bc050
Mangal81 <mangalkumar548744@gmail.com>
2024-08-29 14:36:57 +0530

    file is uploading on local server or local computer, basicaly in Controller/files using localFileUpload controller

--- Controller/FileUpload.js
+++ Controller/FileUpload.js
@@ -1,11 +1,22 @@
 
 
 // uploading file on local computer or server
-exports.localFileUpload = async (req, res) => {
+exports.localFileUpload = async (request, response) => {
 	try {
-		const file = req.files;
+		const file = request.files.file;
+		const path = __dirname +"/files/"+Date.now()+"_"+file.name;
+		file.mv(path, (error)=>{
+			console.log(error);
+		});
 
-		console.log(file);
+		response.json({
+			success:true,
+			data:file,
+			path:path,
+			message:"file uploaded successfully"
+		})
+		console.log(path);
+
 	} catch (error) {
 		console.log(error);
 	}
