2d79c223e077d735bb30ac75575256cd772cc2b5
Mangal81 <mangalkumar548744@gmail.com>
2024-08-31 12:51:36 +0530

    function created to send mail after saving data in db and uploading file on cloudinary

--- Model/File.js
+++ Model/File.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const transporter = require("../Config/mailConfig");
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
 
 const files = mongoose.Schema({
 	name:{
@@ -20,6 +23,30 @@ const files = mongoose.Schema({
 		type:String,
 		required:true,
 		trim:true,
+	}
+})
+
+files.post("save", async function (doc){
+	try {
+
+		// console.log(doc);
+
+		//task 1 = send mail according to image and video (attach video or img tag in html according to file(img or video))
+
+		const result = await transporter.sendMail({
+			from:"Cloudinary file upload",
+			to:doc.email,
+			subject:"File",
+			html:`<h2>Hey.. ${doc.name} File Uploaded successfully</h2> <a href = "${doc.url}">View here</a> </br> </br>
+			 <img src="${doc.url}" alt="uploaded image" width="600" height="400"> 
+			`
+		});
+
+		// console.log(result);
+		console.log("Mail sent");
+	} 
+	catch (error) {
+		console.log(error);
 	}
 })
 
